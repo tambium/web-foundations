27096248cb20332b5dc3d2c4811a60f15fa2b0cc
Dominic Chapman <dominicchapman@users.noreply.github.com>
2020-08-19 15:11:15 +0100

    expose fresh configs

--- packages/eslint-plugin/index.js
+++ packages/eslint-plugin/index.js
@@ -2,7 +2,14 @@ module.exports = {
   rules: {},
 
   configs: {
+    core: require("./lib/config/core"),
+    esnext: require("./lib/config/esnext"),
+    react: require("./lib/config/react"),
     typescript: require("./lib/config/typescript"),
+
+    graphql: require("./lib/config/graphql"),
+    node: require("./lib/config/node"),
+
     prettier: require("./lib/config/prettier"),
   },
 };
