b0e74a51a99ea6432163e28dc2deed75cc9b66b9
Dominic Chapman <dominicchapman@users.noreply.github.com>
2020-08-20 20:46:24 +0100

    ditch babel/* rules

--- packages/eslint-plugin/lib/config/rules/babel.js
+++ packages/eslint-plugin/lib/config/rules/babel.js
@@ -5,19 +5,19 @@
 
 module.exports = {
   // ignores capitalized decorators
-  "babel/new-cap": ["error", { newIsCap: true, capIsNew: false }],
+  "babel/new-cap": "off",
   // doesn't complain about optional chaining
-  "babel/camelcase": ["error", { properties: "always" }],
+  "babel/camelcase": "off",
   // doesn't fail when inside class properties
-  "babel/no-invalid-this": "error",
+  "babel/no-invalid-this": "off",
   // doesn't complain about export x from "mod"; or export * as x from "mod";
-  "babel/object-curly-spacing": ["error", "never"],
+  "babel/object-curly-spacing": "off",
   // doesn't complain about JSX fragment shorthand syntax (<>foo</>;)
-  "babel/quotes": "error",
+  "babel/quotes": "off",
   // doesn't fail when using for await (let something of {})
-  "babel/semi": "error",
+  "babel/semi": "off",
   // doesn't fail when using do expressions or optional chaining
-  "babel/no-unused-expressions": "error",
+  "babel/no-unused-expressions": "off",
   // doesn't complain when used with BigInt
-  "babel/valid-typeof": "error",
+  "babel/valid-typeof": "off",
 };
